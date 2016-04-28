6ffa4bd9b7562434ba15971fbe0cd39763428808
Jaakko Ojalehto <jaakko.ojalehto@yle.fi>
2016-04-28 16:36:12 +0300

    Add get top news by tag

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ import { Wit } from 'node-wit'
 import axios from 'axios'
 import _ from 'lodash'
 
+let entitiesGlobal = []
+
 const actions = {
   say(sessionId, context, message, cb) {
     console.log(message);
     cb();
   },
   merge(sessionId, context, entities, message, cb) {
+    entitiesGlobal[sessionId] = entities
     cb(context);
   },
   error(sessionId, context, err) {
@@ -18,6 +21,16 @@ const actions = {
       .then(response => {
         context.topNews = response.data.data[0].title
         cb(context)
+      })
+  },
+  getTopNewsByTag(sessionId, context, cb) {
+    axios.get('https://www.dr.dk/tjenester/mimer/api/v2/articles/latest.json?limit=1&tags[]=' + entitiesGlobal[sessionId].news_category[0].value)
+      .then(response => {
+        context.topNewsByTag = response.data.data[0].title
+        cb(context)
+      })
+      .catch(response => {
+        console.log(response)
       })
   }
 };
