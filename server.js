ce29aeeede25a0fd918e72952be9076842bda04e
Jaakko Ojalehto <jaakko.ojalehto@yle.fi>
2016-04-28 19:34:10 +0300

    Merge slack server to news tips

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
+import Botkit from 'botkit'
 import { Wit } from 'node-wit'
 import axios from 'axios'
 import _ from 'lodash'
 
-let entitiesGlobal = []
+var controller = Botkit.slackbot({
+    debug: true,
+});
+
+var bot = controller.spawn({
+    token: 'xoxb-38606867408-PmyVviEk9zqZpu9S06VO1bnx'
+}).startRTM();
+
+const entitiesGlobal = []
+const sessionsGlobal = {}
 let newsTips = []
 
 const actions = {
   say(sessionId, context, message, cb) {
-    console.log(message);
+    const session = sessionsGlobal[sessionId]
+    bot.reply(session, message)
     cb();
   },
   merge(sessionId, context, entities, message, cb) {
-    entitiesGlobal[sessionId] = _.assign({}, entitiesGlobal[sessionId], entities)
+    console.log(entities)
+    entitiesGlobal[sessionId] = _.assign({}, getEntities(sessionId), entities)
     cb(context);
   },
-  error(sessionId, context, err) {
-    console.log(err.message);
+  error(sessionId, context, error) {
+    console.log(error.message);
   },
   getTopNews(sessionId, context, cb) {
     axios.get('https://www.dr.dk/tjenester/mimer/api/v2/articles/latest.json?limit=1')
@@ -23,34 +35,46 @@ const actions = {
         context.topNews = response.data.data[0].title
         cb(context)
       })
+      .catch(response => {
+        console.trace(response)
+      })
   },
   getTopNewsByTag(sessionId, context, cb) {
-    axios.get('https://www.dr.dk/tjenester/mimer/api/v2/articles/latest.json?limit=1&tags[]=' + entitiesGlobal[sessionId].news_category[0].value)
-      .then(response => {
-        context.topNewsByTag = response.data.data[0].title
-        cb(context)
-      })
-      .catch(response => {
-        console.log(response)
-      })
+    if(!_.isUndefined(getEntities(sessionId).news_category)) {
+      axios.get('https://www.dr.dk/tjenester/mimer/api/v2/articles/latest.json?limit=1&tags[]=' + entitiesGlobal[sessionId].news_category[0].value)
+        .then(response => {
+          context.topNewsByTag = response.data.data[0].title
+          cb(context)
+        })
+        .catch(response => {
+          console.trace(response)
+        })
+    } else {
+      cb(context)
+    }
   },
   sendNewsTip(sessionId, context, cb) {
-    newsTips.push(entitiesGlobal[sessionId].news_tip[0].value);
+    console.log(getEntities(sessionId))
+    newsTips.push(getEntities(sessionId).news_tip[0].value);
     cb();
   }
-};
-
-const firstEntityValue = (entities, entity) => {
-  const val = entities && entities[entity] &&
-          Array.isArray(entities[entity]) &&
-          entities[entity].length > 0 &&
-          entities[entity][0].value
-      ;
-  if (!val) {
-    return null;
-  }
-  return typeof val === 'object' ? val.value : val;
-};
+}
 
 const client = new Wit('TGTM7ZQAJRZLVNGS5WU7MERPJUSRDZET', actions);
-client.interactive();
+
+controller.on(['direct_mention', 'direct_message'], function(bot, message) {
+  console.log(message)
+  const context1 = {}
+  sessionsGlobal[message.user + message.channel || ''] = message
+  client.runActions(message.user + message.channel || '', message.text, context1, (e, context2) => {
+    if (e) {
+      console.log('Oops! Got an error: ' + e);
+      return;
+    }
+    console.log('The session state is now: ' + JSON.stringify(context2));
+  })
+})
+
+function getEntities(sessionId) {
+  return entitiesGlobal[sessionId] || {}
+}
